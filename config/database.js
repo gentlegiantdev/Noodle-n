d8530a86f104e215b6a548208e43cd187b53de04
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2022-09-04 18:49:03 -0400

    feat: update ejs

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+// require("dotenv").config({ path: "./config/.env" });
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
@@ -6,14 +8,14 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
-      useCreateIndex: true
-    })
+      useCreateIndex: true,
+    });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
