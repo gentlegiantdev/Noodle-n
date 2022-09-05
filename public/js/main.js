01a3eb4b181548d8e55d5482b5d713eb2b08e729
LovethTee <lovegula@yahoo.com>
2022-09-05 13:01:11 -0600

    updated hamburgers and home page

--- public/js/main.js
+++ public/js/main.js
@@ -114,4 +114,12 @@ async function increaseQuantity(){
         console.log(err)
     }
  }
- 
+ /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
