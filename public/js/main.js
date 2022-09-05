a050dcf37cdbf3c4efe62ce3cca6afe77b292a9b
LovethTee <lovegula@yahoo.com>
2022-09-05 00:04:30 -0600

    updated ejs files

--- public/js/main.js
+++ public/js/main.js
@@ -115,3 +115,12 @@ async function decreaseQuantity() {
     console.log(err);
   }
 }
+ /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+ function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
